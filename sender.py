1a6b0c90ac194dbf915dfed0f1995ae641367cb5
gulyaeve <baddman@list.ru>
2022-12-22 14:59:38 +0300

    Changed sender

--- sender.py
+++ sender.py
@@ -14,60 +14,59 @@ async def sender_from_db():
     for item in range(postcard_count):
         await asyncio.sleep(randint(10, 15))
         postcard = await postcards_db.select_postcard(item + 1)
-        log(INFO, f"{postcard['id']=}")
-        user_who_send = await staff.select_employee(id=postcard['user_id_who_sent'])
-        user_to_send = await staff.select_employee(id=postcard['user_id_to_sent'])
-        file = await bot.download_file_by_id(postcard['file_id'])
+        log(INFO, f"{postcard.id=}")
+        user_who_send = await staff.select_employee(id=postcard.user_id_who_sent)
+        user_to_send = await staff.select_employee(id=postcard.user_id_to_sent)
+        file = await bot.download_file_by_id(postcard.file_id)
         me = await get_bot_info()
         if user_who_send is not None:
-            user_who_sent_name = user_who_send.full_name()
-            if user_to_send['telegram_id'] is not None:
+            if user_to_send.telegram_id is not None:
                 try:
                     await bot.send_photo(
-                        user_to_send['telegram_id'],
-                        postcard['file_id'],
-                        caption=f"{user_who_sent_name} отправляет вам благодарность"
+                        user_to_send.telegram_id,
+                        postcard.file_id,
+                        caption=f"{user_who_send.full_name()} отправляет вам благодарность"
                     )
-                    log(INFO, f"Success send message [{user_to_send['telegram_id']}]")
+                    log(INFO, f"Success send message [{user_to_send.telegram_id}]")
                 except:
-                    log(INFO, f"Failed send message [{user_to_send['telegram_id']}] [{postcard['file_id']}]")
-            if user_to_send['email'] is not None:
+                    log(INFO, f"Failed send message [{user_to_send.telegram_id}] [{postcard.file_id}]")
+            if user_to_send.email is not None:
                 try:
                     await send_email_photo(
-                        user_to_send['email'],
+                        user_to_send.email,
                         'Вам отправлена благодарность',
-                        f"{user_who_sent_name} отправляет вам благодарность.\n"
+                        f"{user_who_send.full_name()} отправляет вам благодарность.\n"
                         f"Хочешь получать и создавать спасибки в телеграм, "
                         f"авторизуйся здесь >> https://t.me/{me.username}/",
                         file.getbuffer().tobytes()
                     )
-                    log(INFO, f"Success send email [{user_to_send['email']}]")
+                    log(INFO, f"Success send email [{user_to_send.email}]")
                 except:
-                    log(INFO, f"Failed send email [{user_to_send['email']}]")
+                    log(INFO, f"Failed send email [{user_to_send.email}]")
         else:
-            if user_to_send['telegram_id'] is not None:
+            if user_to_send.telegram_id is not None:
                 try:
                     await bot.send_photo(
-                        user_to_send['telegram_id'],
-                        postcard['file_id'],
+                        user_to_send.telegram_id,
+                        postcard.file_id,
                         caption=f"Вам отправлена анонимная благодарность"
                     )
-                    log(INFO, f"Success send message [{user_to_send['telegram_id']}]")
+                    log(INFO, f"Success send message [{user_to_send.telegram_id}]")
                 except:
-                    log(INFO, f"Failed send message [{user_to_send['telegram_id']}]")
-            if user_to_send['email'] is not None:
+                    log(INFO, f"Failed send message [{user_to_send.telegram_id}] [{postcard.file_id}]")
+            if user_to_send.email is not None:
                 try:
                     await send_email_photo(
-                        user_to_send['email'],
+                        user_to_send.email,
                         'Вам отправлена благодарность',
                         f'Вам отправлена анонимная благодарность.\n'
                         f'Хочешь получать и создавать спасибки в телеграм, '
                         f'авторизуйся здесь >> https://t.me/{me.username}/',
                         file.getbuffer().tobytes()
                     )
-                    log(INFO, f"Success send email [{user_to_send['email']}]")
+                    log(INFO, f"Success send email [{user_to_send.email}]")
                 except:
-                    log(INFO, f"Failed send email [{user_to_send['email']}]")
+                    log(INFO, f"Failed send email [{user_to_send.email}]")
     # await notify_admins(f"Было отправлено {postcard_count} открыток")
 
 
--- utils/db_api/poscard_db.py
+++ utils/db_api/poscard_db.py
@@ -1,6 +1,22 @@
 import asyncio
+import datetime
+from dataclasses import dataclass
+
+import asyncpg
 
 from utils.db_api.db import Database
+
+
+@dataclass
+class Postcard:
+    id: int
+    user_id_who_sent: int
+    user_id_to_sent: int
+    file_id: str
+    raw_file: bytes
+    time_created: datetime.datetime
+    time_sended_telegram: datetime.datetime | None
+    time_sended_email: datetime.datetime | None
 
 
 class PostcardsDB(Database):
@@ -17,10 +33,25 @@ class PostcardsDB(Database):
             user_id_to_sent bigint,
             file_id text,
             raw_file bytea,
-            time_created timestamp DEFAULT now()
+            time_created timestamp DEFAULT now(),
+            time_sended_telegram timestamp DEFAULT NULL,
+            time_sended_email timestamp DEFAULT NULL
         );
         """
         await self._execute(sql, execute=True)
+
+    @staticmethod
+    async def _format_postcard(record: asyncpg.Record) -> Postcard:
+        return Postcard(
+            id=record['id'],
+            user_id_who_sent=record['user_id_who_sent'],
+            user_id_to_sent=record['user_id_to_sent'],
+            file_id=record['file_id'],
+            raw_file=record['raw_file'],
+            time_created=record['time_created'],
+            time_sended_telegram=record['time_sended_telegram'],
+            time_sended_email=record['time_sended_email'],
+        )
 
     async def insert_postcard(self, user_id_who_sent, user_id_to_send, file_id, raw_file):
         sql = "INSERT INTO postcards (user_id_who_sent, user_id_to_sent, file_id, raw_file)" \
@@ -33,4 +64,5 @@ class PostcardsDB(Database):
 
     async def select_postcard(self, id):
         sql = "SELECT * FROM postcards WHERE id=$1"
-        return await self._execute(sql, id, fetchrow=True)
+        result = await self._execute(sql, id, fetchrow=True)
+        return await self._format_postcard(result)
